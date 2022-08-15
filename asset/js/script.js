415df53b191680c3d84b0f0aaf5f468625276c71
aklimaruhina <aklimacse10@gmail.com>
2022-08-15 08:24:24 +0600

    commit

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,137 +1,187 @@
-var $swiperSelector = $('.slider-conainer');
-
-$swiperSelector.each(function(index) {
-    var $this = $(this);
-    $this.addClass('swiper-sliders-' + index);
-    
-    var dragSize = $this.data('drag-size') ? $this.data('drag-size') : 50;
-    var freeMode = $this.data('free-mode') ? $this.data('free-mode') : false;
-    var loop = $this.data('loop') ? $this.data('loop') : false;
-    var slidesDesktop = $this.data('slides-desktop') ? $this.data('slides-desktop') : 4;
-    var slidesTablet = $this.data('slides-tablet') ? $this.data('slides-tablet') : 4;
-    var slidesMobile = $this.data('slides-mobile') ? $this.data('slides-mobile') : 2.5;
-    var spaceBetween = $this.data('space-between') ? $this.data('space-between'): 20;
-    var watchOverflow = $this.data('watch-overflow') ? $this.data('watch-overflow') : true;
-    var carouselPrevButton = $(this).parent().find('.carousel-navigation-prev');
-    console.log(carouselPrevButton);
-    var carouselNextButton = $(this).parent().find('.carousel-navigation-next');
-    var swiper = new Swiper('.swiper-sliders-' + index, {
-      direction: 'horizontal',
-      loop: loop,
-      freeMode: freeMode,
-      watchOverflow: watchOverflow,
-      spaceBetween: spaceBetween,
-      loopFillGroupWithBlank: true,
-      
-      breakpoints: {
-        1920: {
-          slidesPerView: slidesDesktop,
-          slidesPerGroup: slidesDesktop,
-        },
-        992: {
-          slidesPerView: slidesTablet,
-          slidesPerGroup: slidesTablet,
-        },
-        480: {
-           slidesPerView: slidesMobile,
-           slidesPerGroup: slidesMobile
-        }
-      },
-      
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'custom',
-        renderCustom: function (swiper, current, total) {
-
-            return current + '-' + (total - 1)+' of '+ swiper.slides.length; 
-        }
-
-      },
-      navigation: {
-        nextEl: carouselNextButton,
-        prevEl: carouselPrevButton
-      },
-      scrollbar: {
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: false,
+  direction: 'horizontal',
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-page",
+    clickable: true,
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+  scrollbar: {
         el: '.swiper-scrollbar',
         draggable: true,
         // dragSize: dragSize
       },
-
-   });
-    // console.log(swiper.slides.length);
-
 });
-var $swiperSelector2 = $('.slider-2');
 
-$swiperSelector2.each(function(index) {
-    var $this = $(this);
-    $this.addClass('swiper-slider-' + index);
+var swiper2 = new Swiper(".Swipercategory", {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  loop: false,
+  direction: 'horizontal',
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-page",
+    clickable: true,
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+  scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+        // dragSize: dragSize
+      },
+});
+
+// var $swiperSelector = $('.slider-conainejjjir');
+
+// $swiperSelector.each(function(index) {
+//     var $this = $(this);
+//     $this.addClass('swiper-sliders-' + index);
     
-    var dragSize = $this.data('drag-size') ? $this.data('drag-size') : 50;
-    var freeMode = $this.data('free-mode') ? $this.data('free-mode') : false;
-    var loop = $this.data('loop') ? $this.data('loop') : false;
-    var slidesDesktop = $this.data('slides-desktop') ? $this.data('slides-desktop') : 6;
-    var slidesTablet = $this.data('slides-tablet') ? $this.data('slides-tablet') : 4;
-    var slidesMobile = $this.data('slides-mobile') ? $this.data('slides-mobile') : 3.5;
-    var spaceBetween = $this.data('space-between') ? $this.data('space-between'): 20;
-    var watchOverflow = $this.data('watch-overflow') ? $this.data('watch-overflow') : true;
+//     var dragSize = $this.data('drag-size') ? $this.data('drag-size') : 50;
+//     var freeMode = $this.data('free-mode') ? $this.data('free-mode') : false;
+//     var loop = $this.data('loop') ? $this.data('loop') : false;
+//     var slidesDesktop = $this.data('slides-desktop') ? $this.data('slides-desktop') : 4;
+//     var slidesTablet = $this.data('slides-tablet') ? $this.data('slides-tablet') : 4;
+//     var slidesMobile = $this.data('slides-mobile') ? $this.data('slides-mobile') : 2.5;
+//     var spaceBetween = $this.data('space-between') ? $this.data('space-between'): 20;
+//     var watchOverflow = $this.data('watch-overflow') ? $this.data('watch-overflow') : true;
+//     var carouselPrevButton = $(this).parent().find('.carousel-navigation-prev');
+//     console.log(carouselPrevButton);
+//     var carouselNextButton = $(this).parent().find('.carousel-navigation-next');
+//     var swiper = new Swiper('.swiper-sliders-' + index, {
+//       direction: 'horizontal',
+//       loop: loop,
+//       freeMode: freeMode,
+//       watchOverflow: watchOverflow,
+//       spaceBetween: spaceBetween,
+//       loopFillGroupWithBlank: true,
+      
+//       breakpoints: {
+//         1920: {
+//           slidesPerView: slidesDesktop,
+//           slidesPerGroup: slidesDesktop,
+//         },
+//         992: {
+//           slidesPerView: slidesTablet,
+//           slidesPerGroup: slidesTablet,
+//         },
+//         480: {
+//            slidesPerView: slidesMobile,
+//            slidesPerGroup: slidesMobile
+//         }
+//       },
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         type: "fraction",
+//       },
+//       // pagination: {
+//       //   el: '.swiper-pagination',
+//       //   type: 'custom',
+//       //   renderCustom: function (swiper, current, total) {
+
+//       //       return current + '-' + (total - 1)+' of '+ swiper.slides.length; 
+//       //   }
+
+//       // },
+//       navigation: {
+//         nextEl: carouselNextButton,
+//         prevEl: carouselPrevButton
+//       },
+//       scrollbar: {
+//         el: '.swiper-scrollbar',
+//         draggable: true,
+//         // dragSize: dragSize
+//       },
+
+//    });
+//     // console.log(swiper.slides.length);
+
+// });
+
+// var $swiperSelector2 = $('.myslider-2');
+
+// $swiperSelector2.each(function(index) {
+//     var $this = $(this);
+//     $this.addClass('swiper-slider-' + index);
+    
+//     var dragSize = $this.data('drag-size') ? $this.data('drag-size') : 50;
+//     var freeMode = $this.data('free-mode') ? $this.data('free-mode') : false;
+//     var loop = $this.data('loop') ? $this.data('loop') : false;
+//     var slidesDesktop = $this.data('slides-desktop') ? $this.data('slides-desktop') : 6;
+//     var slidesTablet = $this.data('slides-tablet') ? $this.data('slides-tablet') : 4;
+//     var slidesMobile = $this.data('slides-mobile') ? $this.data('slides-mobile') : 3.5;
+//     var spaceBetween = $this.data('space-between') ? $this.data('space-between'): 20;
+//     var watchOverflow = $this.data('watch-overflow') ? $this.data('watch-overflow') : true;
   
-    var swiper = new Swiper('.swiper-slider-' + index, {
-      direction: 'horizontal',
-      loop: loop,
-      freeMode: freeMode,
-      watchOverflow: watchOverflow,
-      spaceBetween: spaceBetween,
-      loopFillGroupWithBlank: true,
-      slidesPerView: 6,
-      slidesPerGroup: 6,
-      // breakpoints: {
-      //   1920: {
-      //     slidesPerView: 6,
-      //     slidesPerGroup: 6,
-      //   },
-      //   992: {
-      //     slidesPerView: slidesTablet,
-      //     slidesPerGroup: slidesTablet,
-      //   },
-      //   480: {
-      //      slidesPerView: slidesMobile,
-      //      slidesPerGroup: slidesMobile
-      //   }
-      // },
+//     var swiper = new Swiper('.swiper-slider-' + index, {
+//       direction: 'horizontal',
+//       loop: loop,
+//       freeMode: freeMode,
+//       watchOverflow: watchOverflow,
+//       spaceBetween: spaceBetween,
+//       loopFillGroupWithBlank: true,
+//       slidesPerView: 6,
+//       slidesPerGroup: 6,
+//       // breakpoints: {
+//       //   1920: {
+//       //     slidesPerView: 6,
+//       //     slidesPerGroup: 6,
+//       //   },
+//       //   992: {
+//       //     slidesPerView: slidesTablet,
+//       //     slidesPerGroup: slidesTablet,
+//       //   },
+//       //   480: {
+//       //      slidesPerView: slidesMobile,
+//       //      slidesPerGroup: slidesMobile
+//       //   }
+//       // },
       
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'custom',
-        renderCustom: function (swiper, current, total) {
+//       pagination: {
+//         el: '.swiper-pagination',
+//         type: 'custom',
+//         renderCustom: function (swiper, current, total) {
 
-            return current + '-' + (total - 1)+' of '+ swiper.slides.length; 
-        }
+//             return current + '-' + (total - 1)+' of '+ swiper.slides.length; 
+//         }
 
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      },
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
-        // dragSize: dragSize
-      },
+//       },
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev'
+//       },
+//       scrollbar: {
+//         el: '.swiper-scrollbar',
+//         draggable: true,
+//         // dragSize: dragSize
+//       },
 
-   });
-    console.log(swiper.slides.length);
+//    });
+//     console.log(swiper.slides.length);
 
-});
-var swiper2 = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    effect: "fade",
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
+// });
+// var swiper2 = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     effect: "fade",
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
     
-  });
+//   });
 $(window).scroll(function() {
   if ($(this).scrollTop() > 100) {
     $('#toTopBtn').fadeIn();
